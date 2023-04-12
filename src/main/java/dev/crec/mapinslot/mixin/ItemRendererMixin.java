0c091b25b29dee5204c55ed05d5c1dd43ef13903
Ella <59192970+Voxxin@users.noreply.github.com>
2023-04-12 15:39:15 -0400

    1.19.4 fix

--- src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
+++ src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
@@ -1,11 +1,17 @@
 package dev.crec.mapinslot.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.crec.mapinslot.MapInSlot;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,17 +19,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
-public class ItemRendererMixin {
-	@Shadow
-	public float blitOffset;
+public abstract class ItemRendererMixin implements ResourceManagerReloadListener {
+
+	@Shadow public abstract BakedModel getModel(ItemStack itemStack, @Nullable Level level, @Nullable LivingEntity livingEntity, int i);
 
 	@Inject(
-		method = "renderGuiItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
+		method = "tryRenderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
 		at = @At("HEAD")
 	)
-	private void drawMap(Font font, ItemStack stack, int x, int y, String string, CallbackInfo ci) {
+	private void drawMap(PoseStack poseStack, LivingEntity livingEntity, Level level, ItemStack stack, int x, int y, int z, int l, CallbackInfo ci) {
 		if (stack.is(Items.FILLED_MAP)) {
-			MapInSlot.renderMap(Minecraft.getInstance(), this.blitOffset, stack, x, y);
+			float blitOffset = (50 + (this.getModel(stack, level, livingEntity, z).isGui3d() ? l : 0));
+			MapInSlot.renderMap(Minecraft.getInstance(), blitOffset, stack, x, y);
 		}
 	}
 }
