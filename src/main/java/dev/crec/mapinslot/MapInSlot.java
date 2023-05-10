6c09cf61871651edde30c6a6c007879696ecb348
Ella <59192970+Voxxin@users.noreply.github.com>
2023-05-10 18:58:30 -0400

    Merge pull request #2
    * 1.19.4 fix
    
    * Forgot to write that it didn't support older versions.
    
    * I don't get it either. But it works now
    
    * Forgot to remove my testing calls
    
    * Made it less redundant
    
    * No longer hides amount of maps.

--- src/main/java/dev/crec/mapinslot/MapInSlot.java
+++ src/main/java/dev/crec/mapinslot/MapInSlot.java
@@ -9,19 +9,23 @@ import net.minecraft.world.item.MapItem;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 
 public class MapInSlot {
-	public static void renderMap(Minecraft mc, float blitOffset, ItemStack stack, int x, int y) {
-		if (mc.player == null) return;
 
-		Integer mapId = MapItem.getMapId(stack);
-		MapItemSavedData saveData = MapItem.getSavedData(mapId, mc.player.level);
+	public static void renderMap(PoseStack poseStack, Minecraft minecraft, float blitOffset, ItemStack itemStack, int x, int y) {
+		if (minecraft.player == null) {
+			return;
+		}
 
-		if (mapId != null && saveData != null) {
+		Integer mapId = MapItem.getMapId(itemStack);
+		MapItemSavedData savedData = MapItem.getSavedData(mapId, minecraft.player.level);
+
+		if (mapId != null && savedData != null) {
 			MultiBufferSource.BufferSource bufferSource = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
-			PoseStack poseStack = new PoseStack();
-			poseStack.translate(x, y, blitOffset + 190);
-			poseStack.scale(1/8F, 1/8F, 1);
-			mc.gameRenderer.getMapRenderer().render(poseStack, bufferSource, mapId, saveData, true, 0xF000D2);
+			poseStack.pushPose();
+			poseStack.translate(x, y, blitOffset + 150);
+			poseStack.scale(1 / 8F, 1 / 8F, 1);
+			minecraft.gameRenderer.getMapRenderer().render(poseStack, bufferSource, mapId, savedData, true, 0xF000D2);
 			bufferSource.endBatch();
+			poseStack.popPose();
 		}
 	}
 }
--- src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
+++ src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
@@ -1,11 +1,17 @@
 package dev.crec.mapinslot.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.crec.mapinslot.MapInSlot;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,17 +19,32 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
-public class ItemRendererMixin {
-	@Shadow
+public abstract class ItemRendererMixin implements ResourceManagerReloadListener {
+
 	public float blitOffset;
 
+	@Shadow public abstract BakedModel getModel(ItemStack itemStack, @Nullable Level level, @Nullable LivingEntity livingEntity, int i);
+
+
 	@Inject(
-		method = "renderGuiItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
+			method = "tryRenderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
+			at = @At("HEAD")
+	)
+	private void drawMap(PoseStack poseStack, LivingEntity livingEntity, Level level, ItemStack stack, int x, int y, int z, int l, CallbackInfo ci) {
+		if (stack.is(Items.FILLED_MAP)) {
+			BakedModel bakedModel = this.getModel(stack, (Level)null, livingEntity, z);
+			blitOffset = (float)(50 + (bakedModel.isGui3d() ? l : 0));
+		}
+	}
+
+
+	@Inject(
+		method = "renderGuiItemDecorations(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
 		at = @At("HEAD")
 	)
-	private void drawMap(Font font, ItemStack stack, int x, int y, String string, CallbackInfo ci) {
+	private void drawMap(PoseStack poseStack, Font font, ItemStack stack, int x, int y, String string, CallbackInfo ci) {
 		if (stack.is(Items.FILLED_MAP)) {
-			MapInSlot.renderMap(Minecraft.getInstance(), this.blitOffset, stack, x, y);
+			MapInSlot.renderMap(poseStack, Minecraft.getInstance(), blitOffset, stack, x, y);
 		}
 	}
 }
