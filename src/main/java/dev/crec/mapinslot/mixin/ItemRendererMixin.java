2ac3e20eeff18a2e67ffb5744a985e8062d2ae4b
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-05-11 19:43:12 -0700

    - Strip down not required code - More straightforward mixin

--- src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
+++ src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
@@ -1,53 +1,52 @@
 package dev.crec.mapinslot.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import dev.crec.mapinslot.MapInSlot;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.MapItem;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin implements ResourceManagerReloadListener {
 
-	@Unique
-	private float blitOffset;
-
-	@Shadow
-	public abstract BakedModel getModel(ItemStack itemStack, @Nullable Level level, @Nullable LivingEntity livingEntity, int i);
-
+	@Shadow @Final
+	private Minecraft minecraft;
 
 	@Inject(
-			method = "tryRenderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
-			at = @At("HEAD")
+		method = "tryRenderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/item/ItemStack;IILnet/minecraft/client/resources/model/BakedModel;)V",
+			shift = Shift.AFTER
+		)
 	)
-	private void drawMap(PoseStack poseStack, LivingEntity livingEntity, Level level, ItemStack stack, int x, int y, int z, int l, CallbackInfo ci) {
-		if (stack.is(Items.FILLED_MAP)) {
-			BakedModel bakedModel = this.getModel(stack, null, livingEntity, z);
-			this.blitOffset = (float)(50 + (bakedModel.isGui3d() ? l : 0));
-		}
-	}
+	private void drawMap(PoseStack poseStack, LivingEntity livingEntity, Level level, ItemStack stack, int x, int y, int k, int l, CallbackInfo ci) {
+		var player = this.minecraft.player;
+		if (player == null || !stack.is(Items.FILLED_MAP)) return;
 
+		var mapId = MapItem.getMapId(stack);
+		var savedData = MapItem.getSavedData(mapId, player.level);
 
-	@Inject(
-		method = "renderGuiItemDecorations(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
-		at = @At("HEAD")
-	)
-	private void drawMap(PoseStack poseStack, Font font, ItemStack stack, int x, int y, String string, CallbackInfo ci) {
-		if (stack.is(Items.FILLED_MAP)) {
-			MapInSlot.renderMap(poseStack, Minecraft.getInstance(), this.blitOffset, stack, x, y);
-		}
+		if (savedData == null) return;
+
+		var bufferSource = this.minecraft.renderBuffers().bufferSource();
+		poseStack.pushPose();
+		// Item model is rendered at offset of 100, we render map on top at 101 in order for it to not have overlapping issues
+		poseStack.translate(x, y, 101);
+		poseStack.scale(0.125F, 0.125F, 1F);
+		this.minecraft.gameRenderer.getMapRenderer().render(poseStack, bufferSource, mapId, savedData, true, 0xF000F0);
+		bufferSource.endBatch();
+		poseStack.popPose();
 	}
 }
