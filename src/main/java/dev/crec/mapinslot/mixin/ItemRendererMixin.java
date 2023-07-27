d20104a404c8dd2d36c8d92e32c4be7f53831fdb
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-07-27 01:11:33 -0700

    Update to 1.20

--- src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
+++ src/main/java/dev/crec/mapinslot/mixin/ItemRendererMixin.java
@@ -2,51 +2,57 @@ package dev.crec.mapinslot.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.MapItem;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ItemRenderer.class)
+@Mixin(GuiGraphics.class)
 public abstract class ItemRendererMixin implements ResourceManagerReloadListener {
 
-	@Shadow @Final
-	private Minecraft minecraft;
+    @Shadow
+    @Final
+    private Minecraft minecraft;
 
-	@Inject(
-		method = "tryRenderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderGuiItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/item/ItemStack;IILnet/minecraft/client/resources/model/BakedModel;)V",
-			shift = Shift.AFTER
-		)
-	)
-	private void drawMap(PoseStack poseStack, LivingEntity livingEntity, Level level, ItemStack stack, int x, int y, int k, int l, CallbackInfo ci) {
-		var player = this.minecraft.player;
-		if (player == null || !stack.is(Items.FILLED_MAP)) return;
+    @Shadow
+    public abstract PoseStack pose();
 
-		var mapId = MapItem.getMapId(stack);
-		var savedData = MapItem.getSavedData(mapId, player.level);
+    @Shadow
+    public abstract MultiBufferSource.BufferSource bufferSource();
 
-		if (savedData == null) return;
+    @Shadow
+    public abstract void flush();
 
-		var bufferSource = this.minecraft.renderBuffers().bufferSource();
-		poseStack.pushPose();
-		// Item model is rendered at offset of 100, we render map on top at 101 in order for it to not have overlapping issues
-		poseStack.translate(x, y, 101);
-		poseStack.scale(0.125F, 0.125F, 1F);
-		this.minecraft.gameRenderer.getMapRenderer().render(poseStack, bufferSource, mapId, savedData, true, 0xF000F0);
-		bufferSource.endBatch();
-		poseStack.popPose();
-	}
+    @Inject(
+            method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
+            at = @At(value = "TAIL")
+    )
+    private void drawMap(Font font, ItemStack stack, int i, int j, String string, CallbackInfo ci) {
+        if (!stack.is(Items.FILLED_MAP)) return;
+
+        var mapId = MapItem.getMapId(stack);
+        var savedData = MapItem.getSavedData(mapId, this.minecraft.level);
+
+        if (savedData == null) return;
+
+        this.pose().pushPose();
+        this.pose().translate(i, j, 200F);
+        this.pose().scale(0.125F, 0.125F, 1F);
+        this.minecraft
+            .gameRenderer
+            .getMapRenderer()
+            .render(this.pose(), this.bufferSource(), mapId, savedData, true, 15728880);
+        this.flush();
+        this.pose().popPose();
+    }
+
 }
